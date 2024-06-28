49b196383f6efb27c0324ba47f811bd033f01e49
Nugget2468 <christianwinnitzk@gmail.com>
2024-06-28 09:29:30 -0400

    updated code

--- assignment2/the-greatest-showdown1.py
+++ assignment2/the-greatest-showdown1.py
@@ -11,6 +11,8 @@ elif number2 >= number1 and number2 >= number3:
 else:
     largest = number3
 
+    
+
 # Determine the smallest number
 if number1 <= number2 and number1 <= number3:
     smallest = number1
